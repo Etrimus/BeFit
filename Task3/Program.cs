fa70ff0300e79827f05c330bacf9065d19292f03
a.rats <a.rats@reglab.ru>
2023-03-31 09:58:20 +0500

    Тестовое задание BeFit

---
+++ Task3/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task3;
+
+internal class Program
+{
+    private static void Main()
+    {
+        var clerk3 = new Clerk { Name = "3" };
+        var clerk4 = new Clerk { Name = "4" };
+        var clerk2 = new Clerk { Name = "2", DependsOn = new List<Clerk> { clerk3, clerk4 } };
+        var clerk1 = new Clerk { Name = "1", DependsOn = new List<Clerk> { clerk2 } };
+
+        var clerks = new List<Clerk> { clerk1, clerk2, clerk3, clerk4 };
+
+        var result = new Dictionary<string, Clerk>();
+
+        foreach (var clerk in clerks)
+        {
+            _processClerks(clerk, ref result);
+        }
+    }
+
+    private static void _processClerks(Clerk clerk, ref Dictionary<string, Clerk> result)
+    {
+        foreach (var dClerk in clerk.DependsOn)
+        {
+            _processClerks(dClerk, ref result);
+        }
+
+        if (!result.ContainsKey(clerk.Name))
+        {
+            result.Add(clerk.Name, clerk);
+        }
+    }
+}
+
+internal class Clerk
+{
+    public IList<Clerk> DependsOn = new List<Clerk>();
+
+    public string Name;
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Task4;
+
+internal class Program
+{
+    private static void Main()
+    {
+        var arr = new[] { 3, 1, 8, 5, 4 };
+
+        Console.WriteLine($"Source array: {string.Join(',', arr)}{Environment.NewLine}");
+
+        for (var i = 0; i <= 10; i++)
+        {
+            _findDesiredNumber(i, arr);
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+
+    private static void _findDesiredNumber(int desiredNumber, int[] sourceArray)
+    {
+        for (var i = 0; i < sourceArray.Length; i++)
+        {
+            for (var l = 0; l < sourceArray.Length; l++)
+            {
+                List<int> c = new() { sourceArray[i] };
+
+                for (var j = l; j < sourceArray.Length; j++)
+                {
+                    if (j == i)
+                    {
+                        j++;
+
+                        if (j >= sourceArray.Length)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (c.Sum() > desiredNumber)
+                    {
+                        continue;
+                    }
+
+                    c.Add(sourceArray[j]);
+
+                    if (c.Sum() == desiredNumber)
+                    {
+                        Console.WriteLine($"desired: {desiredNumber}; result: {string.Join(',', c)}");
+                        return;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine($"desired: {desiredNumber}; result: <false>");
+    }
+}
